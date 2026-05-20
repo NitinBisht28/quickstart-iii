460b3efb79d77621a4d1ee5447880b67d8b58949
NitinBisht28 <nitin.bisht.connect@gmail.com>
2026-05-20 19:40:15 +0530

    script changes

--- quickstart/scripts/setup-caller-worker.sh
+++ quickstart/scripts/setup-caller-worker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cd ~/quickstart/workers/caller-worker
+set -e
+
+sudo apt update
+
+sudo apt install -y curl git
+
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+
+sudo apt install -y nodejs
+
+cd ~/quickstart-iii/quickstart/workers/caller-worker
 
 npm install
