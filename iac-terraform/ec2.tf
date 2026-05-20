9cb2cff8c795b67aaccd0395a7178fd0a9b43d07
NitinBisht28 <nitin.bisht.connect@gmail.com>
2026-05-20 19:29:00 +0530

    iac changes

--- iac-terraform/ec2.tf
+++ iac-terraform/ec2.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "api_vm" {
   ami                    = var.ami_id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.api_sg.id]
-  key_name               = var.key_name
+  key_name               = "aldev"
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
   tags = {
     Name = "api-gateway-vm"
@@ -14,8 +14,8 @@ resource "aws_instance" "caller_worker_vm" {
   ami                    = var.ami_id
   instance_type          = "t3.small"
   subnet_id              = aws_subnet.private.id
-  vpc_security_group_ids = [aws_security_group.api_sg.id]
-  key_name               = var.key_name
+  key_name               = "aldev"
+  vpc_security_group_ids = [aws_security_group.worker_sg.id]
 
   root_block_device {
     volume_size = 8
@@ -31,8 +31,8 @@ resource "aws_instance" "inference_worker_vm" {
   ami                    = var.ami_id
   instance_type          = "t3.large"
   subnet_id              = aws_subnet.private.id
-  vpc_security_group_ids = [aws_security_group.api_sg.id]
-  key_name               = var.key_name
+  key_name               = "aldev"
+  vpc_security_group_ids = [aws_security_group.worker_sg.id]
 
   root_block_device {
     volume_size = 20
--- iac-terraform/security-groups.tf
+++ iac-terraform/security-groups.tf
@@ -1,29 +1,14 @@
-resource "aws_security_group" "api_sg" {
-  name   = "assignment-security-group"
-  vpc_id = aws_vpc.main.id
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-security-group"
+  description = "Security group for bastion VM"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "SSH"
+    description = "SSH from internet"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP API"
-    from_port   = 3111
-    to_port     = 3111
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Internal RPC"
-    from_port   = 49134
-    to_port     = 49134
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
@@ -34,6 +19,47 @@ resource "aws_security_group" "api_sg" {
   }
 
   tags = {
-    Name = "assignment-security-group"
+    Name = "bastion-security-group"
+  }
+}
+
+resource "aws_security_group" "worker_sg" {
+  name        = "worker-security-group"
+  description = "Security group for private worker VMs"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Internal RPC"
+    from_port   = 49134
+    to_port     = 49134
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    description = "Internal HTTP API"
+    from_port   = 3111
+    to_port     = 3111
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    description = "SSH from bastion subnet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "worker-security-group"
   }
 }
